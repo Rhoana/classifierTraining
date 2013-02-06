2a8ca953503dcc7c9319035572922ea056039721
Thouis (Ray) Jones <thouis@seas.harvard.edu>
2013-02-06 14:44:30 -0500

    remove log transform

--- membraneDetection/norm01.m
+++ membraneDetection/norm01.m
@@ -2,12 +2,6 @@ function imNorm = norm01(im)
   im = double(im);
   im = im(:,:,1);
 
-  % invert and log transform, then reinvert
-  im = 256 - im;
-  im(im < 5) = 5;
-  im = log(im);
-  im = - im;
-
   im = im - min(im(:));
   m = max(im(:));
   if m ~= 0
